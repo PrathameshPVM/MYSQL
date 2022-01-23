b6144cd9526d80995edcae0eb56a2bace4f2d7b4
PrathameshPVM <95204156+PrathameshPVM@users.noreply.github.com>
2022-01-23 14:08:36 +0530

    Add files via upload

---
+++ SQLQuery8.sql
@@ -0,0 +1,9 @@
+use mkp2
+create table product(id int primary key,name varchar(20),price int,quantity int)
+insert into product values(1,'Monitor',5000,2)
+insert into product values(2,'CPU',10000,1)
+insert into product values(3,'KeyBoard',600,1)
+insert into product values(4,'Mouse',600,1)
+insert into product values(5,'RAM',600,1)
+select * from product
+
