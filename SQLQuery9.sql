cad650ca6bd20ee61318b7e5523d08620d8c8018
PrathameshPVM <95204156+PrathameshPVM@users.noreply.github.com>
2022-01-23 14:42:01 +0530

    Add files via upload

---
+++ SQLQuery9.sql
@@ -0,0 +1,6 @@
+create table student(rno int primary key,name varchar(20),address varchar(30), mobileno int );
+insert into student values(1,'amit','house 1 nagpur',75898);
+insert into student values(2,'rajesh','nagpur',987654);
+insert into student values(3,'rahul','haydrabad',8759);
+insert into student values(4,'ketan','pune',72325);
+select * from student;
