778ae8416d007921589048c6406bf1047ea1e80f
PrathameshPVM <95204156+PrathameshPVM@users.noreply.github.com>
2022-02-01 15:31:41 +0530

    Add files via upload

--- SQLQuery6.sql
+++ SQLQuery6.sql
@@ -1,1 +1,12 @@
-create table student(rno int, name varchar(20))
+
+CREATE TABLE test_products(
+    product_id INT IDENTITY PRIMARY KEY,
+    product_name VARCHAR(255) NOT NULL,
+    unit_price DEC(10,2),
+    discounted_price DEC(10,2),
+    CHECK(unit_price > 0),
+    CHECK(discounted_price > 0),
+    CONSTRAINT valid_prices1 CHECK(discounted_price < unit_price)
+);
+insert into test_products values('mouse',225,200);
+select * from test_products
