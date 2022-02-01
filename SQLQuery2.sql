d81dccb62b3a048738afbccc4719de1fb04e7bbb
PrathameshPVM <95204156+PrathameshPVM@users.noreply.github.com>
2022-02-01 15:12:08 +0530

    Add files via upload

--- SQLQuery2.sql
+++ SQLQuery2.sql
@@ -1,2 +1,9 @@
-create database mkpits2
-DROP DATABASE mkpits2;
+
+create table products
+(
+product_id int identity primary key,
+product_name varchar(255) not null,
+unit_price dec(10,2) constraint positive_price check(unit_price>0)
+);
+insert into products values('bikes',125)
+select * from products
