c1c0b5b7936dd710233f2d1872e201cb549b2133
PrathameshPVM <95204156+PrathameshPVM@users.noreply.github.com>
2022-02-01 15:20:02 +0530

    Add files via upload

--- SQLQuery4.sql
+++ SQLQuery4.sql
@@ -1,2 +1,16 @@
-create database mkpits2
-DROP DATABASE mkpits2;
+
+
+
+create table products
+(
+product_id int identity primary key,
+product_name varchar(255) not null,
+unit_price dec(10,2) check(unit_price>0),
+discounted_price dec(10,2)check(discounted_price>0),
+check(discounted_price<unit_price)
+);
+insert into products values('bikes',125,20)
+insert into products values('bikes',null,null)
+insert into products values('mouse',220,22)
+
+select * from products
