f149ccc4454ab06a5f6d22beba3338662a2d4cd5
PrathameshPVM <95204156+PrathameshPVM@users.noreply.github.com>
2022-02-01 15:24:27 +0530

    Add files via upload

--- SQLQuery5.sql
+++ SQLQuery5.sql
@@ -1,1 +1,8 @@
-CREATE TABLE sample_db (id int)
+CREATE TABLE test_products(
+    product_id INT IDENTITY PRIMARY KEY,
+    product_name VARCHAR(255) NOT NULL,
+    unit_price DEC(10,2),
+    discounted_price DEC(10,2),
+    CHECK(unit_price > 0),
+    CHECK(discounted_price > 0 AND discounted_price > unit_price)
+	);
