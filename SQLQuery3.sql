142e30296b453e312997229f06c8ba3d215de6d5
PrathameshPVM <95204156+PrathameshPVM@users.noreply.github.com>
2022-02-01 15:13:19 +0530

    Add files via upload

---
+++ SQLQuery3.sql
@@ -0,0 +1,10 @@
+
+create table products
+(
+product_id int identity primary key,
+product_name varchar(255) not null,
+unit_price dec(10,2) constraint positive_price check(unit_price>0)
+);
+insert into products values('bikes',125)
+insert into products values('bikes',null)
+select * from products
