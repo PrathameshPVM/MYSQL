899854db3ffddb760c28e05e7285713a418ad97e
PrathameshPVM <95204156+PrathameshPVM@users.noreply.github.com>
2022-02-01 15:07:21 +0530

    Add files via upload

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,7 +1,9 @@
-SELECT 
-    name
-FROM 
-    master.sys.databases
-ORDER BY 
-    name;
-
+create table products
+(
+product_id int identity primary key,
+product_name varchar(255) not null,
+unit_price dec(10,2)check (unit_price>0)
+);
+insert into products values ('mouse',120)
+insert into products values ('mouse',10)
+select * from products
