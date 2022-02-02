3fc0feeb10e826282f5e6781128d83461cd18532
PrathameshPVM <95204156+PrathameshPVM@users.noreply.github.com>
2022-02-02 16:40:28 +0530

    Add files via upload

--- SQLQuery7.sql
+++ SQLQuery7.sql
@@ -1,9 +1,13 @@
-
-CREATE TABLE products3(
-    product_id INT IDENTITY PRIMARY KEY,
-    product_name VARCHAR(255) NOT NULL,
-    unit_price DEC(10,2) NOT NULL
+create table order1(
+ord_no int primary key,purch_amt dec(10,2),ord_date date,
+customer_id int,salesman_id int
 );
-insert into products3 values('mouse',0)
-insert into products3 values('mouse',0)
-select * from products3
+insert into order1 values(70001,150.5,'2012-10-05',3005,5002);
+insert into order1 values(70009,270.65,'2012-09-10',3001,5005);
+insert into order1 values(70002,65.26,'2012-10-05',3002,5001);
+insert into order1 values(70004,110.5,'2012-08-17',3009,5001);
+insert into order1 values(70007,948.5,'2012-09-10',3005,5003);
+insert into order1 values(70005,2400.6,'2012-07-27',3007,5001);
+insert into order1 values(70008,5760,'2012-09-10',3002,5001);
+select * from order1;
+select ord_no,ord_date,purch_amt from order1 where salesman_id=5001;
