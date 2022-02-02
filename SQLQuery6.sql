87e370a329e39211db2dbb88f7bf1334e5110ea8
PrathameshPVM <95204156+PrathameshPVM@users.noreply.github.com>
2022-02-02 16:33:30 +0530

    Add files via upload

--- SQLQuery6.sql
+++ SQLQuery6.sql
@@ -1,12 +1,14 @@
-
-CREATE TABLE test_products(
-    product_id INT IDENTITY PRIMARY KEY,
-    product_name VARCHAR(255) NOT NULL,
-    unit_price DEC(10,2),
-    discounted_price DEC(10,2),
-    CHECK(unit_price > 0),
-    CHECK(discounted_price > 0),
-    CONSTRAINT valid_prices1 CHECK(discounted_price < unit_price)
+create table customer(
+customer_id int primary key,cust_name varchar(50),city varchar(50),grade int,
+salesman_id int
 );
-insert into test_products values('mouse',225,200);
-select * from test_products
+insert into customer values(3002,'Nick Rimando','New York',100,5001);
+insert into customer values(3007,'Brad Davis','New York',200,5001);
+insert into customer values(3005,'Graham Zusi','California',200,5002);
+insert into customer values(3008,'Julian Green','London',300,5002);
+insert into customer values(3004,'Fabian Johnson','Paris',300,5006);
+insert into customer values(3009,'Geoff Cameron','Berlin',100,5003);
+insert into customer values(3003,'Jozy Altidor','Moscow',200,5007);
+
+select customer_id,cust_name,city,grade,salesman_id from customer
+where grade=200;
