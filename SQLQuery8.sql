5c336fc6c5832fe9a75a0bf5528893965979d49b
PrathameshPVM <95204156+PrathameshPVM@users.noreply.github.com>
2022-02-03 16:10:38 +0530

    Add files via upload

--- SQLQuery8.sql
+++ SQLQuery8.sql
@@ -1,12 +1,35 @@
-create table winner(
-year int,subject varchar(50),winner varchar(50),country varchar(50),
-category varchar(50)
-);
-insert into winner values(1970,'Physics','Hannes Alfven','Sweden','Scientist');
-insert into winner values(1970,'Physics','Louis Neel','France','Scientist');
-insert into winner values(1970,'Chemistry','Luis Federico Leloir','France','Scientist');
-insert into winner values(1970,'Physiology','Ulf von Euler','Sweden','Scientist');
-insert into winner values(1970,'Physiology','Bernard Katz','Germany','Scientist');
-insert into winner values(1970,'Literature','Aleksandr Solzhenitsyn','Russia','Linguist');
-insert into winner values(1970,'Economics','Paul Samuelson','Usa','Economist');
-select year,subject,winner from winner where year=1970;
+CREATE TABLE Fee (   
+      admission_no varchar(45) NOT NULL,  
+      course varchar(45) NOT NULL,      
+      amount_paid int,    
+    );  
+	CREATE TABLE Students (      
+      id int PRIMARY KEY IDENTITY,     
+      admission_no1 varchar(45) NOT NULL,  
+      first_name1 varchar(45) NOT NULL,      
+      last_name1 varchar(45) NOT NULL,  
+      age1 int,  
+      city varchar(25) NOT NULL      
+    );    
+	INSERT INTO Students (admission_no1, first_name1, last_name1, age1, city)       
+    VALUES (3354,'Luisa', 'Evans', 13, 'Texas'),       
+    (2135, 'Paul', 'Ward', 15, 'Alaska'),       
+    (4321, 'Peter', 'Bennett', 14, 'California'),    
+    (4213,'Carlos', 'Patterson', 17, 'New York'),       
+    (5112, 'Rose', 'Huges', 16, 'Florida'),  
+    (6113, 'Marielia', 'Simmons', 15, 'Arizona'),    
+    (7555,'Antonio', 'Butler', 14, 'New York'),       
+    (8345, 'Diego', 'Cox', 13, 'California');  
+	INSERT INTO Fee (admission_no, course, amount_paid)       
+    VALUES (3354,'Java', 20000),       
+    (7555, 'Android', 22000),       
+    (4321, 'Python', 18000),    
+    (8345,'SQL', 15000),       
+    (5112, 'Machine Learning', 30000); 
+	select * from Students
+	select * from fee
+
+	select Students.admission_no1,Students.first_name1,Students.last_name1,
+	fee.course,fee.amount_paid
+	from Students full outer join fee 
+	on Students.admission_no1=Fee.admission_no;
