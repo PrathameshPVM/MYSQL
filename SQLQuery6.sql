03e534b44f561d829ae1bebd7f64364f026611a0
PrathameshPVM <95204156+PrathameshPVM@users.noreply.github.com>
2022-02-03 16:02:12 +0530

    Add files via upload

--- SQLQuery6.sql
+++ SQLQuery6.sql
@@ -1,14 +1,35 @@
-create table customer(
-customer_id int primary key,cust_name varchar(50),city varchar(50),grade int,
-salesman_id int
-);
-insert into customer values(3002,'Nick Rimando','New York',100,5001);
-insert into customer values(3007,'Brad Davis','New York',200,5001);
-insert into customer values(3005,'Graham Zusi','California',200,5002);
-insert into customer values(3008,'Julian Green','London',300,5002);
-insert into customer values(3004,'Fabian Johnson','Paris',300,5006);
-insert into customer values(3009,'Geoff Cameron','Berlin',100,5003);
-insert into customer values(3003,'Jozy Altidor','Moscow',200,5007);
+CREATE TABLE Fee (   
+      admission_no varchar(45) NOT NULL,  
+      course varchar(45) NOT NULL,      
+      amount_paid int,    
+    );  
+	CREATE TABLE Students (      
+      id int PRIMARY KEY IDENTITY,     
+      admission_no1 varchar(45) NOT NULL,  
+      first_name1 varchar(45) NOT NULL,      
+      last_name1 varchar(45) NOT NULL,  
+      age1 int,  
+      city varchar(25) NOT NULL      
+    );    
+	INSERT INTO Students (admission_no1, first_name1, last_name1, age1, city)       
+    VALUES (3354,'Luisa', 'Evans', 13, 'Texas'),       
+    (2135, 'Paul', 'Ward', 15, 'Alaska'),       
+    (4321, 'Peter', 'Bennett', 14, 'California'),    
+    (4213,'Carlos', 'Patterson', 17, 'New York'),       
+    (5112, 'Rose', 'Huges', 16, 'Florida'),  
+    (6113, 'Marielia', 'Simmons', 15, 'Arizona'),    
+    (7555,'Antonio', 'Butler', 14, 'New York'),       
+    (8345, 'Diego', 'Cox', 13, 'California');  
+	INSERT INTO Fee (admission_no, course, amount_paid)       
+    VALUES (3354,'Java', 20000),       
+    (7555, 'Android', 22000),       
+    (4321, 'Python', 18000),    
+    (8345,'SQL', 15000),       
+    (5112, 'Machine Learning', 30000); 
+	select * from Students
+	select * from fee
 
-select customer_id,cust_name,city,grade,salesman_id from customer
-where grade=200;
+	select Students.admission_no1,Students.first_name1,Students.last_name1,
+	fee.course,fee.amount_paid
+	from Students left outer join fee 
+	on Students.admission_no1=Fee.admission_no;
