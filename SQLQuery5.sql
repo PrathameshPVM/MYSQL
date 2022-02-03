24d29b4d5f697be522cb936ac4d14f49b0670beb
PrathameshPVM <95204156+PrathameshPVM@users.noreply.github.com>
2022-02-03 15:29:05 +0530

    Add files via upload

--- SQLQuery5.sql
+++ SQLQuery5.sql
@@ -1,13 +1,5 @@
-create table salesman
-(
-salesman_id int primary key,
-name varchar(50),city varchar(50),commision dec(10,2)
-);
-insert into salesman values (5001,'James Hoogs','New York',0.15); 
-insert into salesman values (5002,'Nail Knite ','Paris',0.13);
-insert into salesman values (5005,'Pit Alex','London',0.11);
-insert into salesman values (5006,'Mc Lyon','Paris',0.14);
-insert into salesman values (5007,'Paul Adam','Rome',0.13);
-insert into salesman values (5003,'Lauson','San Jose',0.12);
+select * from customer
+select * from order1
 
-select name,city  from salesman where city='paris';
+select customer.customer_id,customer.cust_name,order1.customer_id,order1.purch_amt from 
+customer cross join order1 ;
