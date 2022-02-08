5c28cfb62f00f01972c110c8ab7b58691136f689
PrathameshPVM <95204156+PrathameshPVM@users.noreply.github.com>
2022-02-09 00:21:03 +0530

    Add files via upload

--- SQLQuery11.sql
+++ SQLQuery11.sql
@@ -1,16 +1,10 @@
-create table winner(
-year int,subject varchar(50),winner varchar(50),country varchar(50),
-category varchar(50)
-);
-insert into winner values(1970,'Physics','Hannes Alfven','Sweden','Scientist');
-insert into winner values(1970,'Physics','Louis Neel','France','Scientist');
-insert into winner values(1970,'Chemistry','Luis Federico Leloir','France','Scientist');
-insert into winner values(1970,'Physiology','Ulf von Euler','Sweden','Scientist');
-insert into winner values(1970,'Physiology','Bernard Katz','Germany','Scientist');
-insert into winner values(1970,'Literature','Aleksandr Solzhenitsyn','Russia','Linguist');
-insert into winner values(1970,'Economics','Paul Samuelson','Usa','Economist');
-insert into winner values(1971,'Economics','Paul Samuelson','Usa','Economist');
-insert into winner values(1971,'Physiology','Bernard Katz','Germany','Scientist');
-insert into winner values(1971,'Physiology','Dennis Gabor','Germany','Scientist');
-
-select winner from winner where subject='physics' and year >= 1950;
+create proc prod11(@minp as decimal=100,@maxp as decimal=null,@pn as varchar(max))
+as
+begin
+select * from product
+where price >=@minp and (@maxp IS NULL OR price <= @maxp) and name like '%' + @pn+ '%'
+order by name;
+ end;
+ exec prod11 120,200,'mouse'
+exec prod11 120,200,'mouse'
+exec prod11 @minp=100,@pn='e'
