95bce70a7a6c3fdfd9eca1017e63e60c562f517b
PrathameshPVM <95204156+PrathameshPVM@users.noreply.github.com>
2022-02-08 20:21:34 +0530

    Add files via upload

--- SQLQuery2.sql
+++ SQLQuery2.sql
@@ -1,21 +1,7 @@
 select * from product
-create view px
-as select * from product
-where price>600
-
-select * from px;
-
-
-create view p2
-as select id,name from product
-where price>600
-select * from p2;
-
-select * from customer;
-select * from order1;
-
-create view p3a
-as select customer.customer_id,customer.cust_name,order1.ord_date
-from customer inner join order1 on customer.cust_name=order1.customer_id
-
-select * from p3a;
+create procedure prod1a
+as 
+begin
+select name,price from product
+end;
+exec prod1a
