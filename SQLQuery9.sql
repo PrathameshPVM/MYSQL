abf22294f4e1289fa599c321ab150f6f7869c9af
PrathameshPVM <95204156+PrathameshPVM@users.noreply.github.com>
2022-02-09 00:15:34 +0530

    Add files via upload

--- SQLQuery9.sql
+++ SQLQuery9.sql
@@ -1,6 +1,8 @@
-select * from customer
-select * from order1
-
-select customer.customer_id,customer.cust_name,order1.customer_id,order1.purch_amt from 
-customer full outer join order1 
-on customer.customer_id=order1.customer_id;
+create proc prod9(@minp as decimal,@maxp as decimal,@pn as varchar(max))
+as
+begin
+select * from product
+where price >@minp and price<@maxp and name like '%' + @pn+ '%'
+order by name;
+ end;
+ exec prod9 @minp=120,@maxp=700,@pn='key';
