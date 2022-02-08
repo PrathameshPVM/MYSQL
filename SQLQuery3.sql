a1c2b936d457efbdfb4d8497d873299f3d63688a
PrathameshPVM <95204156+PrathameshPVM@users.noreply.github.com>
2022-02-08 20:24:59 +0530

    Add files via upload

--- SQLQuery3.sql
+++ SQLQuery3.sql
@@ -1,5 +1,8 @@
-select * from customer
-select * from order1
 
-select customer.customer_id,customer.cust_name,order1.customer_id,order1.purch_amt from 
-customer inner join order1 on customer.customer_id=order1.customer_id;
+alter procedure prod1
+as 
+begin
+select name,price from product
+order by price
+end;
+exec prod1
